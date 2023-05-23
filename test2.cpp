077d6998278429c52068f21ac659db01b46a4299
souadhassani <134273124+souadhassani@users.noreply.github.com>
2023-05-23 11:23:13 +0200

    Add files via upload

---
+++ test2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h> /*Autorise l'emploi de printf et de scanf.*/
+
+        int main(void)
+        {
+            printf ("test \n");
+            
+            return 0;
+        }
+        
