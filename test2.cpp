48de39ad2fd91db4690580865a0c00adf78f14c4
souadhassani <134273124+souadhassani@users.noreply.github.com>
2023-05-24 12:09:10 +0200

    Delete test2.cpp

--- test2.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> /*Autorise l'emploi de printf et de scanf.*/
-
-        int main(void)
-        {
-            printf ("test \n");
-            
-            return 0;
-        }
-        
